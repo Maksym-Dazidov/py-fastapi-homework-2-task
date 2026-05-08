78f7f279ea33e355951d38013f3db65b480a13d7
Maksym Dazidov <maksymdazidov@gmail.com>
2026-05-08 04:07:18 +0200

    solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -7,7 +7,102 @@ from sqlalchemy.orm import joinedload
 from database import get_db, MovieModel
 from database.models import CountryModel, GenreModel, ActorModel, LanguageModel
 
+from src.schemas.movies import MovieList, MovieCreate, MovieUpdate
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies")
+async def get_movies(
+        db: AsyncSession = Depends(get_db),
+        page: int = Query(default=1, ge=1),
+        per_page: int = Query(default=10, ge=1, le=20)
+):
+    total_items = await db.scalar(select(func.count()).select_from(MovieModel))
+    total_pages = (total_items + per_page - 1) // per_page
+    offset = (page - 1) * per_page
+    result = await db.execute(select(MovieModel).offset(offset).limit(per_page))
+    movies = result.scalars().all()
+
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    prev_page = f"/theater/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"/theater/movies/?page={page + 1}&per_page={per_page}" if page < total_pages else None
+
+    return MovieList(
+        movies=movies,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_items,
+    )
+
+
+@router.get("/movies/{movie_id}")
+async def get_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
+    movie = await db.get(MovieModel, movie_id)
+
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found.")
+
+    return movie
+
+
+@router.post("/movies")
+async def create_movie(movie_data: MovieCreate, db: AsyncSession = Depends(get_db)):
+    movie = MovieModel(
+        name=movie_data.name,
+        date=movie_data.date,
+        score=movie_data.score,
+        overview=movie_data.overview,
+        status=movie_data.status,
+        budget=movie_data.budget,
+        revenue=movie_data.revenue,
+        country=movie_data.country,
+        genres=movie_data.genre,
+        actors=movie_data.actor,
+        languages=movie_data.language,
+    )
+    db.add(movie)
+    await db.commit()
+    await db.refresh(movie)
+    return movie
+
+
+@router.patch("/movies/{movie_id}")
+async def update_movie(movie_id: int, movie: MovieUpdate, db: AsyncSession = Depends(get_db)):
+    movie = await db.get(MovieModel, movie_id)
+
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found.")
+
+    movie.name = movie.name
+    movie.date = movie.date
+    movie.score = movie.score
+    movie.overview = movie.overview
+    movie.status = movie.status
+    movie.budget = movie.budget
+    movie.revenue = movie.revenue
+    movie.country = movie.country
+    movie.genres = movie.genres
+    movie.actors = movie.actors
+    movie.languages = movie.languages
+
+    db.add(movie)
+    await db.commit()
+    await db.refresh(movie)
+    return movie
+
+
+@router.delete("/movies/{movie_id}")
+async def delete_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
+    movie = await db.get(MovieModel, movie_id)
+
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found.")
+
+    movie.delete()
+    await db.commit()
+    await db.refresh(movie)
+    return movie
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,47 @@
-# Write your code here
+import datetime
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict
+
+from src.database.models import CountryModel, GenreModel, ActorModel, LanguageModel
+
+
+class MovieBase(BaseModel):
+    name: str
+    date: datetime.date
+    score: float
+    overview: str
+    status: str
+    budget: float
+    revenue: float
+    country: CountryModel
+    genres: list[GenreModel]
+    actors: list[ActorModel]
+    languages: list[LanguageModel]
+
+
+class MovieDetail(MovieBase):
+    id: int
+    model_config = ConfigDict(from_attributes=True)
+
+
+class MovieList(BaseModel):
+    movies: list[MovieDetail]
+    prev_page: Optional[str]
+    next_page: Optional[str]
+    total_pages: int
+    total_items: int
+
+
+class MovieCreate(MovieBase):
+    pass
+
+
+class MovieUpdate(BaseModel):
+    name: str
+    date: datetime.date
+    score: float
+    overview: str
+    status: str
+    budget: float
+    revenue: float
